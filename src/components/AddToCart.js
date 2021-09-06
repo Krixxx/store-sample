5ee1e0adbfa3c22a2cd06915d5515a0e505bd4f5
Kristjan <krix@planet.ee>
2021-09-06 17:41:37 +0300

    add to cart setup

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,8 @@ import { useCartContext } from '../context/cart_context';
 import AmountButtons from './AmountButtons';
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
+
   const { id, stock, colors } = product;
 
   // main color will be the first color from colors list
@@ -60,7 +62,7 @@ const AddToCart = ({ product }) => {
           increase={increase}
           decrease={decrease}
         />
-        <Link to='/cart' className='btn'>
+        <Link to='/cart' className='btn'onClick={()=>addToCart(id, mainColor, amount, product)}
           add to cart
         </Link>
       </div>
