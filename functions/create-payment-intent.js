29d0f4152427025809c4c37ff6e6c60c5c8cd63e
Kristjan <krix@planet.ee>
2021-09-08 20:06:02 +0300

    stripe checkout - get client secret

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,14 +1,34 @@
 // domain/.netlify/functions/create-payment-intent
 
+// in Node, we must use 'dotenv' pacage to get access to .env file.
+// require in Node is similar to import in React.
+require('dotenv').config();
+
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY);
+
 exports.handler = async function (event, context) {
   if (event.body) {
     const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
-    console.log(cart);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(cart),
+    const calculateOrderAmount = () => {
+      return shipping_fee + total_amount;
     };
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(),
+        currency: 'eur',
+      });
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ msg: error.message }),
+      };
+    }
   }
 
   return {
